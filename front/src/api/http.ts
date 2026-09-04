502da49770d711996823157f1df30654ccfcfec0
Savinien Petitjean <savinien.petitjean@gmail.com>
2026-09-04 13:42:28 +0200

    fix(front): now using a nginx proxy to access the api

--- front/src/api/http.ts
+++ front/src/api/http.ts
@@ -14,7 +14,7 @@ export class ApiError extends Error {
 
 type QueryValue = string | number | boolean | number[] | string[] | undefined;
 
-const API_URL = import.meta.env.VITE_API_URL ?? "http://localhost:3000";
+const API_URL = import.meta.env.VITE_API_URL ?? window.location.href + 'api';
 
 export interface RequestOptions {
   method?: 'GET' | 'POST' | 'PATCH' | 'PUT' | 'DELETE';
