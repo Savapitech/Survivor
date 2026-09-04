d53731fd32d129c19c5392b310f37f3e99e9cf29
Noa Drécourt <noa.drecourt@epitech.eu>
2026-09-04 14:03:36 +0200

    doc(back): add auth's doc

--- back/src/auth/auth.controller.ts
+++ back/src/auth/auth.controller.ts
@@ -10,6 +10,7 @@ import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './local-auth.guard';
 import { LoginUserDto } from './dto/login-user.dto';
 import { Public } from './public.decorateur';
+import { docAuthPost } from './auth.doc';
 
 @Controller('auth')
 export class AuthController {
@@ -18,6 +19,7 @@ export class AuthController {
   @Public()
   @UseGuards(LocalAuthGuard)
   @Post('login')
+  @docAuthPost()
   async login(@Body() dto: LoginUserDto) {
     const user = await this.authService.validateUser(dto.email, dto.password);
     if (!user) {
