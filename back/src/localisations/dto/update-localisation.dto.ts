c094ab5e9b26867054c685288ffa1ccaa7a8024d
Noa Drécourt <noa.drecourt@epitech.eu>
2026-09-06 01:35:11 +0200

    doc(back): document all localisations's route

--- back/src/localisations/dto/update-localisation.dto.ts
+++ back/src/localisations/dto/update-localisation.dto.ts
@@ -1,4 +1,4 @@
-import { PartialType } from '@nestjs/mapped-types';
+import { PartialType } from '@nestjs/swagger';
 import { CreateLocalisationDto } from './create-localisation.dto';
 
 export class UpdateLocalisationDto extends PartialType(CreateLocalisationDto) {}
