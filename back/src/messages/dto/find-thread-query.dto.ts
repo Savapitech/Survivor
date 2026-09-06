57dd980e228b493dc6ad8ac68591cff4715f0bca
Noa Drécourt <noa.drecourt@epitech.eu>
2026-09-06 13:00:02 +0200

    doc(back): document all messages's route

--- back/src/messages/dto/find-thread-query.dto.ts
+++ back/src/messages/dto/find-thread-query.dto.ts
@@ -1,12 +1,25 @@
 import { Type } from 'class-transformer';
 import { IsInt } from 'class-validator';
 import { PaginationQueryDto } from '../../common/pagination';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class FindThreadQueryDto extends PaginationQueryDto {
+  @ApiProperty({
+    description: "recuiter's id",
+    type: Number,
+    example: 1,
+    required: true
+  })
   @Type(() => Number)
   @IsInt()
   recruiterId: number;
 
+  @ApiProperty({
+    description: "seeker's id",
+    type: Number,
+    example: 2,
+    required: true
+  })
   @Type(() => Number)
   @IsInt()
   seekerId: number;
--- back/src/messages/messages.controller.ts
+++ back/src/messages/messages.controller.ts
@@ -11,6 +11,7 @@ import { MessagesService } from './messages.service';
 import { CreateMessageDto } from './dto/create-message.dto';
 import { FindThreadQueryDto } from './dto/find-thread-query.dto';
 import { MarkThreadSeenDto } from './dto/mark-thread-seen.dto';
+import { docMessagesGetConversationsForRecruiter, docMessagesGetConversationsForSeeker, docMessagesGetThread, docMessagesPost, docMessagesPostThreadSeen } from './messages.doc';
 
 @ApiTags('messages')
 @Controller('messages')
@@ -18,16 +19,19 @@ export class MessagesController {
   constructor(private readonly messagesService: MessagesService) {}
 
   @Post()
+  @docMessagesPost()
   create(@Body() dto: CreateMessageDto) {
     return this.messagesService.create(dto);
   }
 
   @Get('thread')
+  @docMessagesGetThread()
   findThread(@Query() query: FindThreadQueryDto) {
     return this.messagesService.findThread(query);
   }
 
   @Get('conversations/recruiter')
+  @docMessagesGetConversationsForRecruiter()
   conversationsForRecruiter(
     @Query('recruiterId', ParseIntPipe) recruiterId: number,
   ) {
@@ -35,11 +39,13 @@ export class MessagesController {
   }
 
   @Get('conversations/seeker')
+  @docMessagesGetConversationsForSeeker()
   conversationsForSeeker(@Query('seekerId', ParseIntPipe) seekerId: number) {
     return this.messagesService.conversationsForSeeker(seekerId);
   }
 
   @Post('seen')
+  @docMessagesPostThreadSeen()
   markThreadSeen(@Body() dto: MarkThreadSeenDto) {
     return this.messagesService.markThreadSeen(dto);
   }
